fbb5f1c912361256e4265f4e48e2bd0bb8b2cf27
Bubbq <soccer17nemo@gmail.com>
2023-09-04 17:58:56 -0500

    Making A Responsive Layout of Main Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        //what our main webpage will look like on different devices, how much area each grid will take depending on the device
+        base: `"nav" "main"`, //on mobile, hides the side panel
+        lg: `"nav nav" "aside main"`, //on larger devices ( > 1024 px)
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg"> {/*The side panel is only shown when the screen is above 'lg' or greater than 1024px*/}
+       
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
